85042bea2d8e33cd68b6469c52be186c6dc3e8ba
Ethan Range <65268454+ethanrange@users.noreply.github.com>
2021-06-21 03:26:06 +0100

    Update grade calculations and redesign footer
    (*) Added calculations for exam results and C Project
    (*) Fixed error in average calculation
    (*) Converted footer to sticky footer

--- calc.js
+++ calc.js
@@ -5,41 +5,78 @@ function setGrades() {
     totalSum = 0;
     weightSum = 0;
 
-    getMark('40001_1', 60, 0.15 * 3 / 11);
-    getMark('40001_2', 100, 0.15 * 5 / 11);
-    getMark('40001_3', 60, 0.15 * 3 / 11);
+    // CW 15% 
+    // Total coursework marks: 220
+    getMark('40001_1', 60, 0.15 * (60 / 220));
+    getMark('40001_2', 100, 0.15 * (100 / 220));
+    getMark('40001_3', 60, 0.15 * (60 / 220));
+    getMark('40001_4', 100, 0.85);
 
-    getMark('40002_1', 20, 0.15 * 1 / 8);
-    getMark('40002_2', 20, 0.15 * 1 / 8);
-    getMark('40002_3', 20, 0.15 * 1 / 8);
-    getMark('40002_4', 20, 0.15 * 1 / 8);
-    getMark('40002_5', 20, 0.15 * 1 / 8);
-    getMark('40002_6', 20, 0.15 * 1 / 8);
-    getMark('40002_7', 20, 0.15 * 1 / 8);
-    getMark('40002_8', 20, 0.15 * 1 / 8);
+    // CW 15% 
+    // Total coursework marks: 160
+    getMark('40002_1', 20, 0.15 * (20 / 160));
+    getMark('40002_2', 20, 0.15 * (20 / 160));
+    getMark('40002_3', 20, 0.15 * (20 / 160));
+    getMark('40002_4', 20, 0.15 * (20 / 160));
+    getMark('40002_5', 20, 0.15 * (20 / 160));
+    getMark('40002_6', 20, 0.15 * (20 / 160));
+    getMark('40002_7', 20, 0.15 * (20 / 160));
+    getMark('40002_8', 20, 0.15 * (20 / 160));
+    getMark('40002_9', 100, 0.85);
 
-    getMark('40003_1', 50, 0.15 * 1);
+    // CW 15% 
+    // Total coursework marks: 50
+    getMark('40003_1', 50, 0.15);
+    getMark('40003_2', 100, 0.85);
 
-    getMark('40004_1', 30, 0.15 * 1);
+    // CW 15% 
+    // Total coursework marks: 30
+    getMark('40004_1', 30, 0.15);
+    getMark('40004_2', 100, 0.85);
 
-    getMark('40005_1', 100, 0.15 * 10 / 17);
-    getMark('40005_2', 70, 0.15 * 7 / 17);
+    // CW 15% 
+    // Total coursework marks: 170
+    getMark('40005_1', 100, 0.15 * (100 / 170));
+    getMark('40005_2', 70, 0.15 * (70 / 170));
+    getMark('40005_3', 100, 0.85);
 
-    getMark('40006_1', 20, 0.15 * 1);
+    // CW 15% 
+    // Total coursework marks: 20
+    getMark('40006_1', 20, 0.15);
+    getMark('40006_2', 100, 0.85);
 
-    getMark('40007_1', 100, 0.15 * 1 / 2);
-    getMark('40007_2', 100, 0.15 * 1 / 2);
+    // CW 15% 
+    // Total coursework marks: 200
+    getMark('40007_1', 100, 0.15 * (100 / 200));
+    getMark('40007_2', 100, 0.15 * (100 / 200));
+    getMark('40007_3', 100, 0.85);
 
-    getMark('40008_1', 20, 0.15 * 1);
+    // CW 15% 
+    // Total coursework marks: 20
+    getMark('40008_1', 20, 0.15);
+    getMark('40008_2', 100, 0.85);
 
-    getMark('40009_1', 20, 15 / 100);
-    getMark('40009_2', 30, 75 / 100);
-    getMark('40009_3', 100, 15 / 100);
-    getMark('40009_4', 90, 105 / 100);
-    getMark('40009_5', 100, 24 / 100);
-    getMark('40009_6', 0, 36 / 100); //BLANK
-    getMark('40009_7', 0, 24 / 100); //BLANK
-    getMark('40009_8', 0, 36 / 100); //BLANK
+    // Each mark is quadrupled as 40009 is weighted as 4 courses
+
+    getMark('40009_1', 20, 4 * (5 / 100)); // Haskell Interim 5%
+    getMark('40009_2', 30, 4 * (25 / 100)); // Haskell Final 25%
+
+    getMark('40009_3', 100, 4 * (5 / 100)); // Video 5%
+    getMark('40009_4', 90, 4 * (5 / 100)); // Report 5%
+
+    getMark('40009_5', 50, 4 * (3 / 100)); // Java Interim A 3%
+    getMark('40009_6', 50, 4 * (2 / 100)); // Java Interim B 2%
+
+    getMark('40009_7', 50, 4 * (18 / 100)); // Java Final A 18%
+    getMark('40009_8', 50, 4 * (17 / 100)); // Java Final B 17%
+
+    getMark('40009_9', 50, 4 * (1 / 100)); // Checkpoint 1%
+    getMark('40009_10', 20, 4 * (12 / 100)); // C Final 12%
+    getMark('40009_11', 5, 4 * (3 / 100)); // C Report / Source 3%
+    getMark('40009_12', 5, 4 * (1 / 100)); // C Extension
+    getMark('40009_13', 5, 4 * (1 / 100)); // C Presentation
+    getMark('40009_14', 5, 4 * (1 / 100)); // C Feedback 1
+    getMark('40009_15', 30, 4 * (1 / 100)); // C Feedback 2
 
     var tsum = document.getElementById('tsum');
     var tweight = document.getElementById('tweight');
